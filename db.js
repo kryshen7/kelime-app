96b20773214a63e626be7e305dedb2b897502fed
kryshen7 <mikailenes0767@gmail.com>
2026-01-19 23:21:46 +0300

    Kelime App initial

---
+++ db.js
@@ -0,0 +1,14 @@
+// db.js
+const mysql = require("mysql2/promise");
+
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "12345",   // MySQL şifre
+  database: "kelime_app",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+module.exports = pool;
